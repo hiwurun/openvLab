ec96bced3cecf6b0fc67053eb8077558f772ca07
zhoulongxi <2572642005@qq.com>
2025-08-21 11:33:18 +0800

    Update Footer component to conditionally render based on active route detection. Integrate useLocation hook to check if the current path is the home page before displaying the footer on non-mobile devices.

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,11 +1,14 @@
 import { useMediaQuery } from '@uidotdev/usehooks';
+import { useLocation } from 'react-router';
 
 export default function Footer() {
   // 是否是移动设备
   const isMobile = useMediaQuery('(max-width: 768px)');
+  const location = useLocation();
+  const hasLoadPage = location.pathname === '/';
   return (
     <>
-      {!isMobile && (
+      {!isMobile && hasLoadPage && (
         <footer className="text-muted-foreground fixed right-0 bottom-0 left-0 border-t bg-gray-50 py-4 text-sm">
           <div className="flex items-center justify-center space-x-2">
             <p>Copyright © 2025 Grace</p>
